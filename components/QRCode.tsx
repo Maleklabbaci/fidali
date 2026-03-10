ce2b6a88ed693053ac9b3ea73465686afc93986f
Maleklabbaci <Marcolabbaci@gmail.com>
2026-03-10 09:52:21 +0100

    Refactor QRCode component with new props and library

--- components/QRCode.tsx
+++ components/QRCode.tsx
@@ -1,16 +1,43 @@
-"use client";
+// components/QRCode.tsx
+'use client'
 
-import QRCode from "react-qr-code";
+import React from 'react'
+import { QRCodeSVG } from 'qrcode.react'
 
-interface Props {
-  value: string;
-  size?: number;
+interface QRCodeProps {
+  value: string
+  size?: number
+  bgColor?: string
+  fgColor?: string
+  logo?: string
 }
 
-export function QRCodeComponent({ value, size = 160 }: Props) {
+export const QRCode: React.FC<QRCodeProps> = ({
+  value,
+  size = 200,
+  bgColor = '#FFFFFF',
+  fgColor = '#000000',
+  logo,
+}) => {
   return (
-    <div style={{ background: "white", padding: 16 }}>
-      <QRCode value={value} size={size} />
+    <div className="inline-flex flex-col items-center gap-2">
+      <QRCodeSVG
+        value={value}
+        size={size}
+        bgColor={bgColor}
+        fgColor={fgColor}
+        level="H"
+        imageSettings={
+          logo
+            ? {
+                src: logo,
+                height: size * 0.2,
+                width: size * 0.2,
+                excavate: true,
+              }
+            : undefined
+        }
+      />
     </div>
-  );
+  )
 }
